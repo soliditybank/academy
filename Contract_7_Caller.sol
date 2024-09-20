587bb6aee83b8c8364211e230e39c02b469c8441
soliditybank.eth <109216723+soliditybank@users.noreply.github.com>
2024-09-21 00:25:46 +0200

    Create Contract_7_Caller.sol

---
+++ Contract_7_Caller.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.27;
+
+contract Callee {
+    uint public x;
+
+    function setX(uint y) external {
+        x = y;
+    }
+}
+
+contract Caller {
+
+    Callee c = Callee(0xddaAd340b0f1Ef65169Ae5E41A8b10776a75482d);
+
+    function foo(uint z) public {
+        c.setX(z);
+    }
+
+}
