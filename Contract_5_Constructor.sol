58a406338cab7e0132f53d121392c549e8bb0d09
soliditybank.eth <109216723+soliditybank@users.noreply.github.com>
2024-09-21 11:29:57 +0200

    Update and rename Contract_5.sol to Contract_5_Constructor.sol

--- Contract_5_Constructor.sol
+++ Contract_5_Constructor.sol
@@ -5,7 +5,7 @@ pragma solidity >=0.8.0 <0.9.0;
 /// @author admin.soliditybank.eth
 /// @title Constructor
 
-contract Contract_5 {
+contract Contract_5_Constructor {
 
     string public name;
     string public symbol;
