96c64bc652123387c3fcdb049db3098832e1a1d8
soliditybank.eth <109216723+soliditybank@users.noreply.github.com>
2024-09-23 21:10:13 +0200

    Create Contract_10_Struct.sol

---
+++ Contract_10_Struct.sol
@@ -0,0 +1,26 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.27;
+
+contract Struct_Test {
+
+    struct User {
+        uint8 age;
+        string name;       
+    }
+
+    User [] users;
+
+    function addUser(string memory _name, uint8 _age) public returns (uint) {
+        users.push(User({age: _age,name: _name}));  
+        return users.length -1;  
+    }
+
+    function getUserAge(uint i) public view returns (uint8) {
+        return users[i].age;
+    }
+
+    function getUserName(uint i) public view returns (string memory) {
+        return users[i].name;
+    }
+
+}
