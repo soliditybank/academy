007f84dc8d619af58e64fa9eec95ed7c214c2462
soliditybank.eth <109216723+soliditybank@users.noreply.github.com>
2024-09-29 22:29:15 +0200

    Create _Global_Variables.sol

---
+++ _Global_Variables.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.27;
+
+contract Global_Variables {
+  function get_block_timestamp() public view returns (uint) {
+    return block.timestamp;
+  }
+}
