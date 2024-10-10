42c548c863f0587c7df39c3de30076f84b3a2f14
soliditybank.eth <109216723+soliditybank@users.noreply.github.com>
2024-10-10 23:25:13 +0200

    Update _Array.sol

--- _Array.sol
+++ _Array.sol
@@ -5,7 +5,7 @@ pragma solidity >=0.8.0 <0.9.0;
 /// @author admin.soliditybank.eth
 /// @title Arrays (Last In, First Out)
 
-contract Contract_3_Array {
+contract Array {
 
 	int256[] public arr;
 	
