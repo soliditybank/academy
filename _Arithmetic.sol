70f2b5fc35dd9011a40995638ff723b676cc2ed5
soliditybank.eth <109216723+soliditybank@users.noreply.github.com>
2024-10-10 23:24:55 +0200

    Update _Arithmetic.sol

--- _Arithmetic.sol
+++ _Arithmetic.sol
@@ -5,7 +5,7 @@ pragma solidity >=0.8.0 <0.9.0;
 /// @author admin.soliditybank.eth
 /// @title Basic arithmetic
 
-contract Contract_2_Arithmetic {
+contract Arithmetic {
 
     function addition(int256 x, int256 y) public pure returns (int256) {
         return x+y;
@@ -22,6 +22,16 @@ contract Contract_2_Arithmetic {
     function division(int256 x, int256 y) public pure returns (int256 quotient, int256 remainder) {
         require(y != 0, "Error: Division by Zero");
         return (x/y, x%y);
+    }
+
+    /**
+     * @dev Returns the average of two signed numbers without overflow.
+     * The result is rounded towards zero.
+     */
+    function average(int256 a, int256 b) internal pure returns (int256) {
+        // Formula from the book "Hacker's Delight"
+        int256 x = (a & b) + ((a ^ b) >> 1);
+        return x + (int256(uint256(x) >> 255) & (a ^ b));
     }
 
 }
