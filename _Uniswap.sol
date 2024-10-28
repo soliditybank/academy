29e43c520cad8dc588e68ffe42acd584b5f4522a
soliditybank.eth <109216723+soliditybank@users.noreply.github.com>
2024-10-28 15:24:21 +0100

    Create _Uniswap.sol

---
+++ _Uniswap.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier
+
+pragma solidity ^0.8.0;
+
+contract Uniswap_Test {
+
+  function slot0() external view returns (
+    uint160 sqrtPriceX96, 
+    int24 tick, 
+    uint16 observationIndex, 
+    uint16 observationCardinality, 
+    uint16 observationCardinalityNext, 
+    uint8 feeProtocol, 
+    bool unlocked
+  )
+
+}
