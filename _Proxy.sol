451f81326edee13530a79e07e8f6625343e2e321
soliditybank.eth <109216723+soliditybank@users.noreply.github.com>
2024-11-01 23:25:08 +0100

    Update _Proxy.sol

--- _Proxy.sol
+++ _Proxy.sol
@@ -2,6 +2,15 @@
 
 pragma solidity ^0.8.0;
 
-contract proxy {
+contract TransparentAdminUpgradeableProxy {
+	address implementation;
+	address admin;
+	fallback() external payable {
+		require(msg.sender != admin);
+		implementation.delegatecall.value(msg.value)(msg.data)
 
+	function upgrade(address newImplementation) external {
+		if (msg.sender != admin) fallback();
+		implementation = newImplementation;
+	}
 }
