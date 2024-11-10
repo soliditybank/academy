11aea4ce16cb33e616addb573549cc601a6b5b2e
soliditybank.eth <109216723+soliditybank@users.noreply.github.com>
2024-11-10 10:07:15 +0100

    Update _Array.sol

--- _Array.sol
+++ _Array.sol
@@ -9,6 +9,18 @@ contract Array {
 
 	int256[] public arr;
 	uint256[] public arr2;
+
+	address[] memory path;
+	if (tokenA == WETH || tokenB == WETH) {
+		path = new address[](2);
+		path[0] = tokenA;
+		path[1] = tokenB;
+	} else {
+		path = new address[](3);
+		path[0] = tokenA;
+		path[1] = WETH;
+		path[2] = tokenB;
+	}
 	
 	function addItem(int256 i) public {
         	arr.push(i);
